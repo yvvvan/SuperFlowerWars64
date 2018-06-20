8aa0b84367a36be8de139febd9b10cc20caa1a92
m.schlensog <m.schlensog@stud.uni-goettingen.de>
2018-06-20 19:43:13 +0200

    Klasse für felder aufm Brett V1.1 (nicht komplett)

--- Field.java
+++ Field.java
@@ -9,27 +9,36 @@ import flowerwarspp.preset.*;
 * @author Maximilian Schlensog
 */
 
-public class Field {
+public class Field extends Flower{
 
   /**Linker Nachbar*/
   private Field left = null;
   /**Rechter Nachbar*/
   private Field right = null;
   /**Unterer Nachbar*/
-  private Field down = null;
-  /**Position auf dem Brett*/
-  private Position [] pos = new Position[3];
-  /**Ist es leer oder hat es ein Blumenbeet?*/
-  private Flower flower = null;
+  private Field vertical = null;
+  /*/**Position auf dem Brett
+  private Position [] pos = new Position[3];*/
   /**Wenn es ein Blumenbeet hat, welcher Farbe ist es?*/
   private PlayerColor color = null;
+  /**Markierung für die legal/illegal Strategie aus der 'Hilfestellung für Implementation'*/
+  private int clustermark = 0;
+
 
   public Field (Position eins, Position zwei, Position drei) {
-
-    pos[0] = eins;
-    pos[1] = zwei;
-    pos[2] = drei;
+    super(eins, zwei, drei);
 
   }//END CONSTRUCTOR
+
+
+  @Override
+  public String toString() {
+    return "Field " + getFirst() + ", " + getSecond() + ", " + getThird() + color + "\n";
+  }//TESTING!!!!!!!!!!!!!!
+
+
+  public void setColor(PlayerColor c) {
+    color = c;
+  }
 
 }//END CLASS
