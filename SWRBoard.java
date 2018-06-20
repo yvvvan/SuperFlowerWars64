259873160b5a87643b649400da0257d6bca1c565
m.schlensog <m.schlensog@stud.uni-goettingen.de>
2018-06-20 19:49:17 +0200

    Brett Klasse v1.1 (nicht komplett) interne darstellung fast komplett

--- SWRBoard.java
+++ SWRBoard.java
@@ -8,7 +8,7 @@ import java.util.*;
 *
 * @author Maximilian Schlensog
 */
-public class SWRBoard implements flowerwarspp.preset.Board {
+public class SWRBoard implements Board, Viewable {
 
   /**Die Groeße (in Dreieckskanten) des Spielbretts*/
   private int size;
@@ -16,23 +16,66 @@ public class SWRBoard implements flowerwarspp.preset.Board {
   private Status status;
   /**Aktueller Spieler*/
   private PlayerColor current = PlayerColor.Red;
-
-//  private HashSet<Int> set;
+  /**Interner Speicher für Flower-menge*/
+  private HashSet<Flower> flowerset;
+  /**Interner Speicher für einzelne Felder auf dem Brett*/
+  private HashSet<Field> fieldset;
+  /**Interner Speicher für moegliche Zuege*/
+  private HashSet<Move> moveset; //SIZE?!?!?!?!?
+  /**Interner Speicher für Graeben-menge*/
+  private HashSet<Ditch> ditchset; //SIZE?!?!?!?!??!
 
   /**Konstruktor, der die Größe des Feldes (in Dreieckskanten) übergeben bekommt*/
   public SWRBoard(int size) {
     if(size >= 3 && size <= 30) {
       this.size = size;
-      //set = new HashSet<Int>(size*size);
+      flowerset = new HashSet(size * size);
+      fieldset = new HashSet(size * size);
+
+      //Initialisierung---------------------------------------------------------
+      for(int i = 1; i <= (size); i++) {
+        int j = 1;
+        for(; j <= (size); j++) {
+          //If you wanna make sense of this, look in early notes: "this exists!"
+          if((i+j) <= (size+1)) {
+            fieldset.add(new Field(new Position(i, j), new Position(i, j+1), new Position(i+1, j)));
+            //STILL GOTTA DO NEIGHBORINOS HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            if((j-1) != 0) {
+              fieldset.add(new Field(new Position(i, j), new Position(i+1, j), new Position(i+1, j-1)));
+            }
+          }
+        }
+      }
+      //ende Init---------------------------------------------------------------
+
+      /*for(Field x : fieldset) {
+        System.out.println(x.toString());
+      }USED FOR TESTING*/
+      //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      Flower f = new Flower(new Position(1,1), new Position(1,2), new Position(2,1));
+      for(Field x : fieldset) {
+        if(x.equals(f)) {
+          x.setColor(current);
+          System.out.println(x.toString());
+        }
+      }//FRIGGIN TESTING OVER HERE!!!!!!!!!!
+
+
     }
     else {
-      System.out.println("Spielbrett kann nicht initialisiert werden.\nSpielbrett Größe muss größergleich 3 und kleinergleich 30 sein.");
+      System.out.println("Spielbrett kann nicht initialisiert werden.\nSpielbrettgröße muss größer als oder gleich 3 und kleiner als oder gleich 30 sein.\n");
     }
   }//END SWRBOARD-CONSTRUCTOR
+  //============================================================================
 
   public PlayerColor getCurrentPlayer() {// TESTING METHOD!!!!!!!!
     return current;
   }
+
+  public void setCurrentPlayer(PlayerColor color) {
+    current = color;
+  }//END SETCURRENTPLAYER
+  //============================================================================
 
   @Override
   public Viewer viewer() {
@@ -40,10 +83,12 @@ public class SWRBoard implements flowerwarspp.preset.Board {
     return new MyViewer(this, current, status);
 
   }//END VIEWER
+  //============================================================================
 
   public int getSize () {
     return size;
   }//END GETSIZE
+  //============================================================================
 
   @Override
   public void make(final Move move) throws IllegalStateException {
@@ -53,36 +98,69 @@ public class SWRBoard implements flowerwarspp.preset.Board {
       case Flower:
         //TO DO
         break;
+        //END CASE FLOWER-------------------------------------------------------------------------
+
       case Ditch:
         //TO DO
         break;
+        //END CASE DITCH-------------------------------------------------------------------------
+
       case Surrender:
-        //TO DO
+        if(current == PlayerColor.Red) {
+          status = Status.BlueWin;
+        }
+        else {
+          status = Status.RedWin;
+        }
         break;
+        //END CASE SURRENDER-------------------------------------------------------------------------
+
       case End:
-        //TO DO
+        if(getPoints(PlayerColor.Red) > getPoints(PlayerColor.Blue)) {
+          status = Status.RedWin;
+        }
+        else if(getPoints(PlayerColor.Red) < getPoints(PlayerColor.Blue)) {
+          status = Status.BlueWin;
+        }
+        else {
+          status = Status.Draw;
+        }
         break;
+        //END CASE END-------------------------------------------------------------------------
+
       default:
-          System.out.println("Make-methode hat keinen Movetype...?");
+        System.out.println("Make-methode hat keinen Movetype...?");
+        break;//DO I NEED IT? I MEAN IT ENDS RIGHT THERE RIGHT?
+        //END DEFAULT CASE------------------------------------------------------
+
     }
 
   }//END MAKE
+  //============================================================================
 
   public Collection<Move> getPossibleMoves() {
         System.out.println("getPossibleMoves got called, but it's not yet implemented :(");
        return null;
   }//END GETPOSSIBLEMOVES
+  //============================================================================
 
   public int getPoints(final PlayerColor color) {
       System.out.println("getPoints got called, but it's not yet implemented :(");
        return 0;
   }//END GETPOINTS
+  //============================================================================
 
   public static void main (String[] args) {
     SWRBoard b = new SWRBoard(3);
     int x = b.getPoints(b.getCurrentPlayer());
+    System.out.println(b.status);
+    Move m = new Move(MoveType.Surrender);
+    b.make(m);
+    System.out.println(b.status);
+
 
   }//END MAIN
+  //============================================================================
 
 
 }//END CLASS
